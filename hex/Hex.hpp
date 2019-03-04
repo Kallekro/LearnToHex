7ab92a915319004e8624e2094e7e17ccc63672de
Kalle Kromann <kallekro@gmail.com>
2019-03-04 22:33:38 +0100

    shit works

--- hex/Hex.hpp
+++ hex/Hex.hpp
@@ -2,6 +2,7 @@
 #define HEX_HPP
 #include <shark/LinAlg/Base.h>
 #include <string>
+#include <memory>
 
 namespace Hex {
 
@@ -11,26 +12,105 @@ namespace Hex {
         Empty = 2
     };
 
-    struct LineSegment {
-        unsigned m_count;
-        bool m_connected_A = false;
-        bool m_connected_B = false;
 
+    class LineSegment {
     public:
-        std::pair<unsigned, unsigned>* TilePositions;
-        unsigned Count() { return m_count; }
-        unsigned ConnectedA() { return m_connected_A; }
-        unsigned ConnectedB() { return m_connected_B; }
-        LineSegment (std::pair<unsigned, unsigned>* tile_positions) {
-            TilePositions = tile_positions;
-            // tile_positions = new std::pair<unsigned, unsigned>[count]
+        int count;
+        bool Connected_A = false;
+        bool Connected_B = false;
+        LineSegment (bool _connected_A, bool _connected_B) {
+            Connected_A = _connected_A;
+            Connected_B = _connected_B;
+            count = 1;
+        }
+
+        bool Merge(std::shared_ptr<LineSegment> other) {
+            Connected_A |= other->Connected_A;
+            Connected_B |= other->Connected_B;
+            return Connected_A && Connected_B;
+        }
+
+        bool Merge(bool other_A, bool other_B) {
+            Connected_A = Connected_A || other_A;
+            Connected_B = Connected_B || other_B;
+            return Connected_A && Connected_B;
+        }
+
+
+    };
+
+    class Tile {
+        std::shared_ptr<LineSegment> m_linesegment;
+        Tile* m_tile_ref;
+    public:
+        TileState tileState;
+        std::shared_ptr<LineSegment> GetLineSegment() {
+            if (m_linesegment != nullptr) {
+                return m_linesegment;
+            } else if (m_tile_ref != nullptr) {
+                return m_tile_ref->GetLineSegment();
+            } else {
+                return nullptr;
+            }
+        }
+
+        LineSegment* ReferLineSegment(Tile* newref) {
+            if (m_linesegment != nullptr) {
+                m_linesegment = nullptr;
+                m_tile_ref = newref;
+            } else if (m_tile_ref != nullptr) {
+                m_tile_ref->ReferLineSegment(newref);
+            }
+        }
+
+        bool OwnsLine() { return m_linesegment != nullptr; }
+
+        Tile () {
+            tileState = Empty;
+            m_linesegment = nullptr;
+            m_tile_ref = nullptr;
+        }
+
+        void free_segment () {
+            if (m_linesegment != nullptr) {
+                m_linesegment = nullptr;
+            }
+        }
+
+        bool PlaceTile(TileState newState, Tile* neighbours[6], int edge_id) {
+            tileState = newState;
+            //std::cout << edge_id << std::endl;
+            bool tile_connected_A = (edge_id == -1);
+            bool tile_connected_B = (edge_id == 1);
+
+            bool foundNeighbour = false;
+            bool won = false;
+            for (int i=0; i < 6; i++) {
+                if (neighbours[i] != nullptr && neighbours[i]->tileState == tileState) {
+                    if (foundNeighbour && neighbours[i]->GetLineSegment() != GetLineSegment()) {
+                        // Another neighbour, combine segments
+                        won = neighbours[i]->GetLineSegment()->Merge(GetLineSegment()) || won;
+                        ReferLineSegment(neighbours[i]);
+                        GetLineSegment();
+                    }
+                    else if (!foundNeighbour) {
+                        // First neighbour, add to this segment
+                        foundNeighbour = true;
+                        won = neighbours[i]->GetLineSegment()->Merge(tile_connected_A, tile_connected_B) || won;
+                        m_tile_ref = neighbours[i];
+                    }
+                }
+            }
+            if (!foundNeighbour) {
+                m_linesegment = std::shared_ptr<LineSegment>(new LineSegment(tile_connected_A, tile_connected_B));
+            }
+            return won;
         }
     };
 
     class Game {
-        static const unsigned BOARD_SIZE = 11;
 
-	    shark::blas::matrix<TileState> m_gameboard;
+	    shark::blas::matrix<Tile> m_gameboard;
         unsigned m_current_player = 0;
 
         const std::string m_red_color = "\033[1;31m";
@@ -42,14 +122,18 @@ namespace Hex {
             "\u2b21"                                 // Empty
         };
 
+        bool m_one_player_debug_mode = false;
+
         void m_next_player() {
-            m_current_player = (m_current_player + 1) % 2;
+            if (!m_one_player_debug_mode) {
+                m_current_player = (m_current_player + 1) % 2;
+            }
         }
 
         void m_initializeboard() {
             for (int i=0; i < BOARD_SIZE; i++) {
                 for (int j=0; j < BOARD_SIZE; j++) {
-                    m_gameboard(i,j) = Empty;
+                    m_gameboard(i,j) = Tile();
                 }
             }
         }
@@ -64,8 +148,37 @@ namespace Hex {
             std::cout << std::endl;
         }
 
-        bool m_place_tile(std::pair<unsigned, unsigned> pos) {
-            m_gameboard(pos.first, pos.second) = (TileState)m_current_player;
+        Tile* m_get_neighbour(int x, int y) {
+            if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE || m_gameboard(x,y).tileState == Empty) {
+                return nullptr;
+            }
+            return &m_gameboard(x,y);
+        }
+
+        bool m_place_tile(std::pair<unsigned, unsigned> pos, bool* won) {
+            Tile* neighbours[6] = {
+                // Upper neigbours
+                m_get_neighbour(pos.first,   pos.second-1),
+                m_get_neighbour(pos.first+1, pos.second-1),
+                // Horizontal neighbours
+                m_get_neighbour(pos.first-1, pos.second),
+                m_get_neighbour(pos.first+1, pos.second),
+                // Lower neighbours
+                m_get_neighbour(pos.first-1, pos.second+1),
+                m_get_neighbour(pos.first,   pos.second+1)
+            };
+            int edge_id = 0;
+            switch (m_current_player) {
+                case 0:
+                    if      (pos.second == 0) { edge_id = -1; }
+                    else if (pos.second == BOARD_SIZE-1) { edge_id = 1; }
+                    break;
+                case 1:
+                    if      (pos.first == 0) { edge_id = -1; }
+                    else if (pos.first == BOARD_SIZE-1) { edge_id = 1; }
+                    break;
+            }
+            return m_gameboard(pos.first, pos.second).PlaceTile((TileState)m_current_player, neighbours, edge_id);
         }
 
         std::pair<unsigned,unsigned> m_alphnum2num(std::string position) {
@@ -73,13 +186,22 @@ namespace Hex {
         }
 
     public:
-        Game() : m_gameboard(BOARD_SIZE, BOARD_SIZE) {
+        Game(bool test_mode) : m_gameboard(BOARD_SIZE, BOARD_SIZE) {
             m_initializeboard();
+            m_one_player_debug_mode = test_mode;
         }
+        static const unsigned BOARD_SIZE = 11;
 
-        void take_turn(std::string pos) {
-            m_place_tile(m_alphnum2num(pos));
+        unsigned CurrentPlayer() {return m_current_player;}
+
+        bool take_turn(std::string pos, bool* won) {
+            std::pair<unsigned,unsigned> pos_pair = m_alphnum2num(pos);
+            if (m_gameboard(pos_pair.first, pos_pair.second).tileState != Empty) {
+                return false;
+            }
+            *won = m_place_tile(pos_pair, won);
             m_next_player();
+            return true;
         }
 
         void printhex( ) {
@@ -91,12 +213,35 @@ namespace Hex {
                 }
                 std::cout << m_blue_color << i+1 << m_reset_color;
                 for (int j=0; j < BOARD_SIZE; j++) {
-                    std::cout << " " << m_hexes[m_gameboard(i, j)];
+                    std::cout << " " << m_hexes[m_gameboard(i, j).tileState];
                 }
                 std::cout << " " << m_blue_color << i+1 << m_reset_color;
                 std::cout << std::endl;
             }
             m_printletters(BOARD_SIZE + 2);
+        }
+
+        void free_segments() {
+            for (int i=0; i < BOARD_SIZE; i++) {
+                for (int j=0; j < BOARD_SIZE; j++) {
+                    m_gameboard(i, j).free_segment();
+                }
+            }
+        }
+
+        void print_segments() {
+            for (int i=0; i < BOARD_SIZE; i++) {
+                for (int j=0; j < BOARD_SIZE; j++) {
+                    if (m_gameboard(i, j).OwnsLine()) {
+                        if (m_gameboard(i,j).tileState == Red) {
+                            std::cout << "Red " << std::endl;
+                        }
+                        else {
+                            std::cout << "Blue " << std::endl;
+                        }
+                    }
+                }
+            }
         }
     };
 }
--- hex/main.cpp
+++ hex/main.cpp
@@ -7,18 +7,217 @@
 #include <shark/ObjectiveFunctions/Benchmarks/Sphere.h>
 #include <vector>
 
-
 #include <shark/Models/LinearModel.h>//single dense layer
 #include <shark/Models/ConvolutionalModel.h>//single convolutional layer
 #include <shark/Models/ConcatenatedModel.h>//for stacking layers, provides operator>>
+/*
+void bluewin(Hex::Game game) {
+    game.printhex();
+    game.take_turn("A1");
+    game.take_turn("A11");
+    game.printhex();
+    game.take_turn("E5");
+    game.take_turn("K1");
+    game.printhex();
+    game.take_turn("B1");
+    game.take_turn("B11");
+    game.printhex();
+    game.take_turn("C1");
+    game.take_turn("C11");
+    game.printhex();
+    game.take_turn("D1");
+    game.take_turn("D11");
+    game.printhex();
+    game.take_turn("E1");
+    game.take_turn("E11");
+    game.printhex();
+    game.take_turn("F1");
+    game.take_turn("F11");
+    game.printhex();
+    game.take_turn("G1");
+    game.take_turn("G11");
+    game.printhex();
+    game.take_turn("H1");
+    game.take_turn("H11");
+    game.printhex();
+    game.take_turn("I1");
+    game.take_turn("I11");
+    game.printhex();
+    game.take_turn("J1");
+    game.take_turn("J11");
+    game.printhex();
+}*/
+#include <stdlib.h>
+#include <random>
+#include <unistd.h>
+
+
+void random_strategy (Hex::Game game) {
+
+    std::random_device rd;
+    std::mt19937 rng_engine(rd());
+    std::uniform_int_distribution<int> dist(1,game.BOARD_SIZE);
+
+    bool won;
+    bool valid_move = true;
+    std::string random_tile = "";
+
+    char letters[game.BOARD_SIZE];
+
+    for (char i = 65; i < 65+game.BOARD_SIZE; i++) {
+        letters[i-65] = i;
+    }
+
+    int turn_count = 0;
+    while (1) {
+        do {
+            if (!valid_move) {
+                std::cout << "Invalid move!" << std::endl;
+            }
+            auto randint1 = dist(rng_engine);
+            auto randint2 = dist(rng_engine);
+            random_tile = letters[randint1-1] + std::to_string(randint2);
+            //std::cout << randint1 << ", " << randint2 << std::endl;
+            //std::cout << random_tile << std::endl;
+        }
+        while (!(valid_move = game.take_turn(random_tile, &won)));
+        game.printhex();
+        //usleep(250000);
+        //game.print_segments();
+        if (won) {
+            std::cout << game.CurrentPlayer() << " LOST" << std::endl;
+            break;
+        }
+        turn_count++;
+        if (turn_count >= 11*11) {
+            std::cout << "Run out of turns, no win?" << std::endl;
+            game.print_segments();
+            break;
+        }
+    }
+}
+
+int test() {
+    Hex::Game game(true);
+    bool won;
+    game.take_turn("D6",&won);
+    game.take_turn("E6",&won);
+    game.take_turn("F6",&won);
+    game.take_turn("G6",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("F5",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("F4",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("E5",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("I5",&won);
+    game.take_turn("I6",&won);
+    game.take_turn("I7",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("H6",&won);
+    game.printhex();
+    game.print_segments();
+}
+
+void test2() {
+    Hex::Game game(true);
+    bool won;
+    game.take_turn("D4",&won);
+    game.take_turn("D5",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("C7",&won);
+    game.take_turn("B8",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("A9",&won);
+    game.take_turn("K6",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("E6",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("F6",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("G6",&won);
+    game.take_turn("H6",&won);
+    game.take_turn("I6",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("J6",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("D6",&won);
+    game.printhex();
+    game.print_segments();
+    std::cout << won << std::endl;
+}
+
+int test2B () {
+        Hex::Game game(true);
+    bool won;
+    game.take_turn("D4",&won);
+    game.take_turn("D5",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("C7",&won);
+    game.take_turn("B8",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("E6",&won);
+    game.take_turn("F6",&won);
+    game.take_turn("G6",&won);
+    game.take_turn("H6",&won);
+    game.take_turn("I6",&won);
+    game.take_turn("J6",&won);
+    game.take_turn("K6",&won);
+    game.printhex();
+    game.print_segments();
+    game.take_turn("D6",&won);
+    game.printhex();
+    game.print_segments();
+    std::cout << won << std::endl;
+}
 
 int main () {
+    Hex::Game game(false);
+    //bluewin(game);
+    random_strategy(game);
+    //test();
+    //test2();
+    //test2B();
 
-    Hex::Game game;
-    game.printhex();
-    game.take_turn("A5");
-    game.take_turn("E5");
-    game.printhex();
 
+    game.free_segments();
+    return 0;
+
+    char input[50];
+    bool won;
+    bool valid_move = true;
+    while (1) {
+        do {
+            if (!valid_move) {
+                std::cout << "Invalid move!" << std::endl;
+            }
+            std::cout << "Turn: ";
+            std::cin >> input;
+        }
+        while (!(valid_move = game.take_turn(input, &won)));
+
+        game.printhex();
+        game.print_segments();
+        if (won) {
+            std::cout << "Won" << std::endl;
+            break;
+        }
+    }
+    game.free_segments();
     return 0;
 }
